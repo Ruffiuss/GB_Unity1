779aa614f8ab17aafd50a277982041e46b3d575b
Ruffiuss <68909837+Ruffiuss@users.noreply.github.com>
2020-10-28 22:09:08 +0500

    Added part of second homework #2
    Собрать свой уровень (по своему концепту). - в прогрессе
    Написать свой скрипт контроллера (передвижение). - сделано
    Реализовать спаунер противников. - сделано

---
+++ Lesson1/Lesson2/Assets/Lesson2_Musagitov_A/Trash/EnemySpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    private GameObject[] Enemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemies = GameObject.FindGameObjectsWithTag("Enem");
+        foreach (var item in Enemies)
+        {
+            Instantiate(_enemy, item.transform.position, item.transform.rotation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Lesson1/Lesson2/Assets/Lesson2_Musagitov_A/Trash/LemonController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class LemonController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2.0f;
+    private Vector3 _moveDirection = Vector3.zero;
+
+    private void Start()
+    {
+        //var rigidBody = gameObject.GetComponent<Rigidbody>();
+        //rigidBody.mass = 2;
+
+        //var collider = gameObject.GetComponent<Collider>();
+        //collider.enabled = false;
+    }
+
+    private void Update()
+    {
+        Move();
+        Jump(); 
+    }
+
+    private void Jump()
+    {
+        //Прыжок
+        if (Input.GetButtonDown("Jump"))
+        {
+            transform.position += transform.up;
+
+        }
+    }
+
+    private void Move()
+    {
+        _moveDirection.x = Input.GetAxis("Horizontal");
+        _moveDirection.z = Input.GetAxis("Vertical");
+        transform.position += _moveDirection * Time.deltaTime * _speed;
+    }
+}
